a82c433f3aa189aa4b021029ce0347b79ad8e63f
RitaSoaresss <151063327+RitaSoaresss@users.noreply.github.com>
2024-07-18 00:28:02 +0100

    Update and rename FASTQC_RNA_2.sh to 1_FASTQC_RNA.sh

--- scripts_RNA_Alignment/1_FASTQC_RNA.sh
+++ scripts_RNA_Alignment/1_FASTQC_RNA.sh
@@ -2,5 +2,5 @@
 for n in $(ls /mnt/nfs/lobo/SALMEIDA-NFS/lcosta/MCPP-Lung/RNASeq/fastq_RNA/fastq_RNA/*_R2_001.fastq.gz)
 do
  echo $n
- sbatch --export=sample=$n,ALL FASTQC_RNA_2.sbatch
+ sbatch --export=sample=$n,ALL 1_FASTQC_RNA.sbatch
 done
