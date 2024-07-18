f3e994b6ad8aa93bbaf1ee36db357fb4d37e1ace
RitaSoaresss <151063327+RitaSoaresss@users.noreply.github.com>
2024-07-18 01:24:35 +0100

    Update and rename fastp.sh to 1_fastp.sh

--- scripts_DNA_Alignment/1_fastp.sh
+++ scripts_DNA_Alignment/1_fastp.sh
@@ -2,5 +2,5 @@
 for n in $(ls /mnt/nfs/lobo/SALMEIDA-NFS/lcosta/MCPP-Sarcoma/raw_data/New_Illumina_sent/Lung/*_R1_001.fastq.gz)
 do
   echo $n
-  sbatch --export=sample=$n,ALL fastp_new_illumina.sbatch
+  sbatch --export=sample=$n,ALL 1_fastp.sbatch
 done
